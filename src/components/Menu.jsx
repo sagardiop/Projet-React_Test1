9589811bcf521be9faaa3918f5601cd51b26a812
sagardiop <sagardiop099@gmail.com>
2025-11-09 20:50:43 +0000

    l

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,9 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Menu() {
   return (
-    <div className="fixed top-0 left-0 w-30 h-screen bg-gray-800 text-white p-4 flex flex-col justify-between">
-      
-     
+    <div className="fixed top-0 left-0 w-33 h-screen bg-gray-800 text-white p-4 flex flex-col justify-between">
       <div>
         <div className="mb-6">
           <img src="/image1/Link → SVG.svg" alt="Logo profil" className="mb-2" />
@@ -29,19 +27,16 @@ function Menu() {
     <Link to="/dashboard/hotelform" className="hover:underline text-xs">Ajouter un hotel</Link>
   </li> */}
 </ul>
-      </div>
+    </div>
+ <div className="flex items-center space-x-2"> 
+      <div className="w-14 h-14 bg-black rounded-full flex items-center justify-center"> 
+       <span className="text-white text-lg">s</span> </div>
+  <div className="flex items-center space-x-1"> 
+    <div className="w-2 h-2 bg-green-500 rounded-full"></div>
+     <h5>ligne</h5>
+  </div>
+</div>
 
-     
-      <div className="mt-10">
-        <div className="border-t-2 border-[#aeaeb2]/70 rounded-full mb-4"></div>
-
-        <div className="flex items-center space-x-3">
-          <div className="w-14 h-14 p-1 rounded-full overflow-hidden -mb-1">
-            <img src="/image1/Picture.png" alt="Profil" className="w-full h-full object-cover rounded-full" />
-          </div>
-          <h5>ligne</h5>
-        </div>
-      </div>
 
     </div>
   );
